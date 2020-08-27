7159dcd8377e25c024a5d4f45d153279ac823ca5
Vitor Prata <39749209+vitorbretasprata@users.noreply.github.com>
2020-08-27 03:16:53 -0300

    adicionar metodos do techer controller

--- app/Controllers/Http/TeachersController.ts
+++ app/Controllers/Http/TeachersController.ts
@@ -2,6 +2,9 @@ import { HttpContextContract } from '@ioc:Adonis/Core/HttpContext';
 import AccountTypeTeacher from "App/Models/AccountTypeTeacher";
 import Database from '@ioc:Adonis/Lucid/Database';
 import Account from 'App/Models/Account';
+import { rules, schema, validator } from '@ioc:Adonis/Core/Validator';
+import AccountTypeStudent from 'App/Models/AccountTypeStudent';
+import ScheduledClass from 'App/Models/ScheduledClass';
 
 export default class TeachersController {
 
@@ -9,7 +12,6 @@ export default class TeachersController {
         try {
             const { filter, page, limit } = request.all();
 
-            // const teachers = await AccountTypeTeacher.query().paginate(page, limit);
             let account : any = "";
             if(filter.lecture_id) {
 
@@ -58,19 +60,138 @@ export default class TeachersController {
         }
     }
 
-    public async schedule() {
+    public async schedule({ response, request, auth } : HttpContextContract) {
         try {
-            
+
+            const { scheduleInfo } = request.all();
+
+            const validationSchema = schema.create({
+                idStudent: schema.number([
+                    rules.required(),
+                ]),
+                idTeacher: schema.number([
+                    rules.required(),
+                ]),
+                date: schema.date({}, [
+                    rules.required()
+                ]),
+                location: schema.string({
+                    escape: true
+                }, [
+                    rules.required()
+                ]),
+                needMovement: schema.boolean([
+                    rules.required()
+                ]),
+                totalValue: schema.number([
+                    rules.required()
+                ])
+            });
+
+            const validatedSchema = await validator.validate({
+                schema: validationSchema,
+                data: scheduleInfo
+            });
+
+            await ScheduledClass.create({
+                ...validatedSchema,
+                status: 1
+            });
+
+            response.ok(200);
+
+        } catch(error) {
+            response.abort({
+                code: 500,
+                default: "Ocorreu um error na hora de listar os professores, tente mais tarde.",
+                detail: error.body
+            });
+        }
+    }
+
+    public async updateScheduledClass({ response, request, auth } : HttpContextContract) {
+        const { status, scheduleId } = request.all();
+
+        try {
+            if(status === 2 || status === 3) {
+                await ScheduledClass.query().where("id", scheduleId).update("status", status);
+            } else {                
+                await ScheduledClass.query().where("id", scheduleId).delete();
+            }
+
+            response.ok(200);
+
+        } catch(error) {
+            response.abort({
+                code: 500,
+                default: "Ocorreu um error na hora de listar os professores, tente mais tarde.",
+                detail: error.body
+            });
+        }
+    }
+
+    public async getClassesDate({ response, request, auth } : HttpContextContract) {
+        try {
+            const { date } = request.all();
+
+            const userId = await this.getUserId(auth.user?.id);
+
+            if(!userId) {
+                response.abort("Usuário não encontrado.");
+            }
+
 
         } catch(error) {
             
         }
     }
 
-    public async CancelClass({ response, request } : HttpContextContract) {
+    public async getUserId(authId) {
         try {
-            // const { filter, page, limit } = request.all();
+            const account = await Account.query().select(["id", "type"]).where("authentication_id", authId).first();
+            let userTypeId;
 
+            if(account?.type === 1) 
+                userTypeId = await AccountTypeStudent.query().select(["id"]).where("student_id", account.id);
+            else
+                userTypeId = await AccountTypeTeacher.query().select(["id"]).where("teacher_id", account.id);
+
+            return userTypeId.id;
+        } catch(error) {
+            console.log(error);
+
+            return false;
+            
+        }
+    }
+
+
+
+    public async getScheduledClass({ response, request } : HttpContextContract) {
+        const { scheduleId } = request.all();
+
+        try {
+            const scheduledClass = ScheduledClass.query().where("id", scheduleId).firstOrFail();
+
+            response.ok(scheduledClass);
+
+        } catch(error) {
+            response.abort({
+                code: 500,
+                default: "Ocorreu um error na hora de listar os professores, tente mais tarde.",
+                detail: error.body
+            });
+        }
+    }
+
+    public async CancelClass({ response, request, auth } : HttpContextContract) {
+        try {
+            const { classId } = request.all();
+            const id = auth.user?.id || -1;
+
+            const userTypeId = await this.getUserId(id);
+
+            await Database.rawQuery(`DELETE FROM schedule_classes WHERE student_id = ${userTypeId} OR teacher_id = ${userTypeId} AND id = ${classId}`);
 
             response.ok(200);
 
--- start/Routes/teachers.ts
+++ start/Routes/teachers.ts
@@ -1,7 +1,10 @@
 import Route from '@ioc:Adonis/Core/Route'
 
 Route.group(() => {
-    Route.post("/listTeachers", "TeachersController.ListTeachers"),
+    Route.get("/listTeachers", "TeachersController.ListTeachers"),
+    Route.post("/getDate", "TeachersController.getClassesDate"),
+    Route.get("/getScheduledClass", "TeachersController.getScheduledClass"),
+    Route.put("/updateScheduledClass", "TeachersController.updateScheduledClass"),
     Route.post("/schedule", "TeachersController.ScheduleClass"),
     Route.post("/cancel", "TeachersController.CancelClass")
 }).prefix("/teachers").middleware('auth');
